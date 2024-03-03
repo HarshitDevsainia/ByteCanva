7b5f4127201cc41565e5fffb4a520a382e446a7b
HarshitDevsainia <hdevsania.in>
2024-03-03 16:59:52 +0530

    Set up the Mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.URL)
+.then(()=>{
+    console.log('DataBase is Connected');
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app=express();
 
