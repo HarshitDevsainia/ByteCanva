fe4bda923c0d18c15e845dea614745166fa7e7b7
HarshitDevsainia <hdevsania.in>
2024-04-30 16:44:09 +0530

    Create the About and Project section

--- BlogApp/src/Pages/About.jsx
+++ BlogApp/src/Pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <h1>Hii Harshit About</h1>
+    <div className="min-h-screen p-28">
+        <h1 className='text-3xl font-medium text-center p-5 text-gray-800 dark:text-white'>About ByteCanva</h1>
+        <div className="text-md flex flex-col flex-wrap gap-2 text-center text-gray-700 dark:text-white">
+          <p>
+            Welcome to Byte Canva, your go-to destination for all things ReactJS, Next.js, and JavaScript-related! 
+          </p>
+          <p>
+            At Byte Canva, we're passionate about sharing knowledge and insights about these cutting-edge technologies that power the dynamic web experiences of today. Whether you're a seasoned developer looking to stay updated with the latest trends or a newcomer eager to dive into the world of web development, our blog is designed to cater to all skill levels.
+          </p>
+          <p>
+            From beginner-friendly tutorials to in-depth analyses of advanced concepts, we cover a wide range of   topics to help you sharpen your skills and stay ahead in the fast-paced world of web development. Our   team of experienced developers and enthusiasts is dedicated to crafting informative and engaging content   that resonates with our diverse audience.
+          </p>
+          <p>
+            Why Byte Canva? We believe in the power of concise yet comprehensive explanations, paired with visually appealing illustrations, to make complex concepts easier to understand. Just like the byte-sized units of data that form the foundation of modern computing, our blog aims to break down complex topics into digestible bytes of knowledge.
+          </p>
+          <p>
+            Whether you're interested in mastering the fundamentals of ReactJS, exploring the versatility of Next.js, or delving into the intricacies of JavaScript, Byte Canva is your ultimate resource for insightful articles, tutorials, and tips. Join us on this journey of learning, discovery, and innovation in the ever-evolving landscape of web development.
+          </p>
+          <p>
+            Get ready to unleash your creativity, enhance your skills, and transform your ideas into reality with Byte Canva!
+          </p>
+        </div>
+    </div>
   )
 }
--- BlogApp/src/Pages/Project.jsx
+++ BlogApp/src/Pages/Project.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import CallToAction from '../Components/CallToAction'
 
 export default function project() {
     return(
         <>
-          <h1>Hii Harshit Project</h1>
+          <div className=" min-h-screen flex flex-col gap-4 items-center justify-center">
+            <h1 className="text-2xl font-semibold text-center">Projects</h1>
+            <p className="text-md text-center text-gray-500 dark:text-white">Build Fun and Engaging while learning HTML,CSS,JavaScript</p>
+            <div className=" max-w-3xl"><CallToAction/></div>
+          </div>
         </>
     )
 }
